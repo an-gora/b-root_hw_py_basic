b3995d1d6f8792aa6316eb750f3ed4bf8bc6ed19
Ana <an.gordievich@gmail.com>
2021-12-20 00:23:10 +0200

    task_01 is done

---
+++ task_03.py
@@ -0,0 +1,8 @@
+# Write a program, which has two print statements
+# to print the following text (capital letters “O” and “H”, made from “#” symbols):
+
+print('\n', '#' * 9, '\n', '#', '\t\t', '#', '\n', '#', '\t\t', '#',
+      '\n', '#', '\t\t', '#', '\n', '#' * 9)
+
+print('\n', '#', '\t\t', '#', '\n', '#', '\t\t', '#', '\n', '#' * 9, '\n',
+      '#', '\t\t', '#', '\n', '#', '\t\t', '#')
