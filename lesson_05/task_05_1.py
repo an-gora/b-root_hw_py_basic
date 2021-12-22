9bcb451380e2314b3a77b377a5e02d11841c1d43
Ana <an.gordievich@gmail.com>
2021-12-22 18:34:15 +0200

    add task 05_1

--- lesson_05/task_05_1.py
+++ lesson_05/task_05_1.py
@@ -2,7 +2,14 @@ from datetime import date
 import calendar
 
 name = "Anastasiia"
-current_day = date.today()
-current_week_day = calendar.day_name[current_day.weekday()]
-print(f"Good day {name}! {current_week_day} is a perfect day " \
-      f"to learn spme python")
+
+
+def task_05_1(name):
+    current_day = date.today()
+    current_week_day = calendar.day_name[current_day.weekday()]
+    return (f"Good day {name}! {current_week_day} is a perfect day " \
+            f"to learn some python")
+
+
+if __name__ == '__main__':
+    print(task_05_1(name))
---
+++ lesson_05/task_05_2.py
@@ -0,0 +1,4 @@
+name = 'Anastasiia'
+surname = 'Gordiievych'
+print('concatenated name + surname = ', ' '.join([name, surname]))
+print('%s %s' % (name, surname))
