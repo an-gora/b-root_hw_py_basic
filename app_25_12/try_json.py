891a6f5ecaaf77a8d466612c34e308f08f3ded35
Ana <an.gordievich@gmail.com>
2022-01-15 02:45:18 +0200

    upd - try to save in json

--- app_25_12/try_json.py
+++ app_25_12/try_json.py
@@ -1,12 +1,17 @@
 import json
+import pathlib
 
+path_to_this_file = pathlib.Path(__file__)
+path_to_parent = path_to_this_file.parent
+path_to_json_with_users = path_to_parent.joinpath('list_of_users.json')
 list_of_users = {
     'user_1': 'tom',
     'user_2': 'mary',
     'user_3': 'bob',
 }
 
-with open('list_of_users.json', 'w') as file_for_users:
+# with open('list_of_users.json', 'w') as file_for_users:
+with open(path_to_json_with_users, 'w') as file_for_users:
     json.dump(list_of_users, file_for_users, indent=4)
 
 
@@ -25,8 +30,6 @@ def add_user(user_list, key_value: dict):
     with open('list_of_users.json', 'w') as file_for_users_in_funct:
         json.dump(user_list, file_for_users_in_funct, indent=4)
     return user_list
-
-
 # если мне не нужен return?
 
 
@@ -38,11 +41,11 @@ def print_json(file_name):
 
 
 def main():
-    print('original dict')
+    print('list_of_users.json after creation')
     print_json('list_of_users.json')
     delete_user(list_of_users, 'user_1')
     add_user(list_of_users, {'user_4': 'tony'})
-    print('dict after deleting and adding')
+    print('list_of_users.json after deleting and adding')
     print_json('list_of_users.json')
 
 
