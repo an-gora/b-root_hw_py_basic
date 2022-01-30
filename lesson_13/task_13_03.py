1086c1ffe86b081d40b0a677589cc740ce83884a
Ana <an.gordievich@gmail.com>
2022-01-30 23:18:29 +0200

    task 13

--- lesson_13/task_13_03.py
+++ lesson_13/task_13_03.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Product:
     def __init__(self, type_of_product: str, name: str, price: int):
         self.type_of_product = type_of_product
@@ -7,29 +10,32 @@ class Product:
     def __repr__(self):
         return f'type of product - {self.type_of_product}, name - {self.name}, original price - {self.price}'
 
+
 class ProductStore:
-    def __init__(self, name_of_store):
-        self.name_of_store = name_of_store
+    def __init__(self):
         self.all_products = {}
 
     def add(self, product, amount):
         store_price = product.price * 1.3
-        self.all_products[product] = (store_price, amount)
+        self.all_products[product.name] = (product.type_of_product, product.price, store_price, amount)
+        print(f'{product.name} in amount of {amount} was added')
 
     # def set_discount(self, identifier, percent, identifier_type=’name’):
     # def sell_product(self, product_name, amount):
-    # def get_income(self):
+    # # def get_income(self):
     def get_all_products(self):
-        for k,v in self.all_products.items():
-            return f'{k}. in this shop price and amount {v}'
-
-    def get_product_info(self, product_name):
         for k, v in self.all_products.items():
-            if k[self.name] == product_name:
-                return k, v
+            return f'{k}. In this shop price and amount {v}'
+
+    # def get_product_info(self, product_name):
+    #     for k, v in self.all_products.items():
+    #         for i in k:
+    #             if i == product_name:
+    #                 return k
+
 
 p = Product('Sport', 'Football T-Shirt', 100)
-s = ProductStore('new store')
+s = ProductStore()
 s.add(p, 10)
-# print(s.get_all_products())
-print(s.get_product_info('Football T-Shirt'))
+print(s.get_all_products())
+# print(s.get_product_info('Football T-Shirt'))
