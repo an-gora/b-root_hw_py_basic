fde49525dc86fefd5716eb9dc856d55d0fe5eea6
Ana <an.gordievich@gmail.com>
2022-02-04 22:53:03 +0200

    task 25

--- lesson_25/task_25_05.py
+++ lesson_25/task_25_05.py
@@ -10,9 +10,8 @@
 def sum_of_digits(digit_string: str) -> int:
     if int(digit_string) <=9:
         return digit_string
-    # return sum_of_digits() + int(digit_string)
     return (int(digit_string))%10 + sum_of_digits((int(digit_string)//10))
 
 
-print(sum_of_digits('12345'))
+print(sum_of_digits('123456'))
 
