722f5e43907a55e8f361f9eeb36ffcd5ab9cd47f
Ana <an.gordievich@gmail.com>
2022-02-04 22:47:45 +0200

    task 25

--- lesson_25/task_25_05.py
+++ lesson_25/task_25_05.py
@@ -6,7 +6,13 @@
 #     >>> sum_of_digits('test')
 #     ValueError("input string must be digit string")
 
+
 def sum_of_digits(digit_string: str) -> int:
-    if len(digit_string) == 1:
+    if int(digit_string) <=9:
         return digit_string
-    return sum_of_digits() + int(digit_string)
+    # return sum_of_digits() + int(digit_string)
+    return (int(digit_string))%10 + sum_of_digits((int(digit_string)//10))
+
+
+print(sum_of_digits('12345'))
+
