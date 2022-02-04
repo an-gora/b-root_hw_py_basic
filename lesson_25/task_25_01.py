c206078888a77e8deed809110d106d875d3b1f91
Ana <an.gordievich@gmail.com>
2022-02-04 17:04:26 +0200

    task 25

--- lesson_25/task_25_01.py
+++ lesson_25/task_25_01.py
@@ -1,1 +1,9 @@
-# ValueError: This function works only with exp > 0
+def to_power(x, exp: int):
+    if exp < 0:
+        raise ValueError('This function works only with exp > 0')
+    if x == 0 or exp == 0:
+        return 1
+    return to_power(x, exp - 1) * x
+
+
+print(to_power(2, 3))
