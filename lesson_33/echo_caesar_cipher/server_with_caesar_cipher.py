26f54e03d7a708cb7db822cb5433835d36ecb756
Ana <an.gordievich@gmail.com>
2022-02-19 01:58:24 +0200

    task 33

--- lesson_33/echo_caesar_cipher/server_with_caesar_cipher.py
+++ lesson_33/echo_caesar_cipher/server_with_caesar_cipher.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = '127.0.0.1'  # Standard loop-back interface address (localhost)
-PORT = 65431  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65421  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.bind((HOST, PORT))
@@ -11,15 +11,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     with conn:
         print('Connected by', addr)
         while True:
-            # data.decode('utf-8')
-
             data = conn.recv(1024)
             data_to_cipher = 'tree'
-            # alphabet = ' abcdefghijklmnopqrstuvwxyz'
-            # res = ''
-            # for char in data_to_cipher:
-            #     res += alphabet[(alphabet.index(char) + data) % len(alphabet)]
-            # print('Result: "' + res + '"')
+            alphabet = ' abcdefghijklmnopqrstuvwxyz'
+            res = ''
+            for char in data_to_cipher:
+                res += alphabet[(alphabet.index(char) + data) % len(alphabet)]
+            print('Result: "' + res + '"')
             if not data:
                 break
-            conn.sendall(data_to_cipher.encode('utf-8'))
+            conn.sendall(res)
