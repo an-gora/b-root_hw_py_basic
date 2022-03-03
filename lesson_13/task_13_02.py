952ac5f9bd290fd048a6083dc2b1bd978d316c0d
Ana <an.gordievich@gmail.com>
2022-03-03 22:13:54 +0200

    task 34

--- lesson_13/task_13_02.py
+++ lesson_13/task_13_02.py
@@ -4,34 +4,31 @@
 
 
 class Mathematician:
-
     pass
 
-    def square_nums(self,list_of_int:list):
+    def square_nums(self, list_of_int: list):
         list_of_square = []
         for this_int in list_of_int:
             list_of_square.append(this_int ** 2)
         return list_of_square
 
-    def remove_positives(self, list_of_int:list):
+    def remove_positives(self, list_of_int: list):
         list_without_positive = []
         for this_int in list_of_int:
             if this_int < 0:
                 list_without_positive.append(this_int)
         return list_without_positive
 
-    def filter_leaps(self, list_of_years:list):
+    def filter_leaps(self, list_of_years: list):
         list_of_not_leap_years = []
         for this_int in list_of_years:
-            if this_int % 4==0:
+            if this_int % 4 == 0:
                 list_of_not_leap_years.append(this_int)
         return list_of_not_leap_years
+
 
 m = Mathematician()
 result1 = m.square_nums([7, 11, 5, 4])
 result2 = m.remove_positives([26, -11, -8, 13, -90])
 result3 = m.filter_leaps([2001, 1884, 1995, 2003, 2020])
 print(result3)
-
-
-
