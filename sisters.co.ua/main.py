13a5b5565b1f1eef281cfd59f2b2caf9ed193203
Ana <an.gordievich@gmail.com>
2022-04-16 17:57:34 +0300

    shop

--- sisters.co.ua/main.py
+++ sisters.co.ua/main.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
@@ -15,10 +16,10 @@ class Product:
     brand: str
     img_link: str
     link_to_product: str
+    img_path: str
+    id: int
 
     volume: str = ''
-    img_path: str = ''
-    id: int = 0
     origin: str = ''
     gender: str = ''
 
@@ -49,24 +50,28 @@ def pars_main(url: str) -> list:
         price = int(re.sub("[^0-9]", "", card.find('div', class_='card-price').text.strip()))
         img_link = BASE_URL + card.find('div', class_='card-image').find('a').attrs['href']
         brand = card.find('div', class_='card-brand').text.strip()
-        product_list.append(Product(name, category, price, brand, img_link, link))
+        img_path = bs.find('div', class_='card-image').find('img').attrs['src']
+        id = bs.find('div', class_='card-footer').find('button').attrs['data-id']
+        product_list.append(Product(name, category, price, brand, img_link, link, img_path, id))
     print(product_list)
     print(url)
     return product_list
 
+
 def pars_extra(product:Product):
     res = requests.get(product.link_to_product)
     bs = BeautifulSoup(res.text, 'lxml')
-    volume= bs.find()
-    img_path=
-    id=
-    origin=
-    gender=
-
-
-
-
-
+    table = bs.find_all('tr')
+    for elem in table:
+        if elem.find('th').text == 'Об\'єм':
+            product.volume = elem.find('td').text.strip()
+        elif elem.find('th').text == 'Країна':
+            product.origin = elem.find('td').text.strip()
+        elif elem.find('th').text == 'Стать':
+            product.gender = elem.find('td').text.strip()
+    out_list = []
+    out_list.append(product)
+    return out_list
 
 
 
@@ -83,27 +88,36 @@ def grabber(links: list, parser):
         elements = []
         threads = []
         for page in links:
-            threads.append(executor.submit(pars_main, url=page))
+            threads.append(executor.submit(parser, page))
         for page in as_completed(threads):
             elements += page.result()
     print(elements)
     return elements
 
 
-
 def collect_main_data():
     base_category_url = 'https://sisters.co.ua/kosmetyka-dlya-volossa/'
     pages_in_cat = pages_in_category(base_category_url)
     links_list = []
-    for page in range(1, pages_in_cat+1):
+    for page in range(1, pages_in_cat + 1):
         links_list.append(base_category_url+'page/'+str(page))
-    grabber(links_list)
+    products = grabber(links_list, pars_main)
+    full_products = grabber(products, pars_extra)
+    return full_products
 
 
-def collect_extra_data(data):
-    pass
+def to_json(data: list):
+    general_list = []
+    for item in data:
+        general_list.append(item.__dict__)
+    with open('products.json', 'w') as file:
+        json.dump(general_list, file, indent = 4)
 
 
-
-
-print(grabber())
+list_of_shamp = collect_main_data()
+# collect_main_data()
+print('-----------')
+for item in list_of_shamp:
+    print(item)
+print(len(list_of_shamp))
+to_json(list_of_shamp)
